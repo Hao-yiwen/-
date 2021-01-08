722be8dda3fb7ed6676d08c8fe7177352bef9f28
haoyiwen <haoyiwen@kuaishou.com>
2021-01-08 15:55:21 +0800

    完成整体架构设计

---
+++ message_board/src/Comment.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react'
+import CommentInput from './CommentInput';
+
+export default class Comment extends Component{
+    render(){
+        return(
+            <div>Comment</div>
+        )
+    }
+}
